0df136f321e6a8babfe17a3a10927ae5149dfd4e
Raghib Quader <quaderr@mcmaster.ca>
2023-04-25 20:25:09 -0400

    Bug fixes and add hold switch
    Add hold switch
    
    Adjust max/min range for pot value so only 1 to 99 shown on the
    display and it doesn't overflow.
    
    Add delay to main loop so dice "rolling" only happens at 10Hz.

--- dice.ino
+++ dice.ino
@@ -1,18 +1,19 @@
 #include "SevSeg.h"
 
-#define MIN_POT 0
-#define MAX_POT 1023
-#define MIN_RANGE 0
+#define MIN_POT 23
+#define MAX_POT 1000
+#define MIN_RANGE 1
 #define MAX_RANGE 99
 
 const int POTENTIOMETER = A0;
 const int TILT_SENSOR = A2;
 const int LED = A4;
+const int HOLD_SWITCH = A5;
 
 const byte NUM_DIGITS = 4;
 const byte DIGIT_PINS[] = {2, 3, 4, 5}; // 1, 2, 3, 4
 
-                          // A, B,  C, D, E, F,  G, DP
+                          // A, B, C, D,  E,  F,  G, DP
 const byte SEGMENT_PINS[] = {6, 7, 8, 9, 10, 11, 12, 13};
 
 int displayValue = 3;
@@ -21,13 +22,13 @@ SevSeg display;
 
 void setup()
 {
-  cli();  //stop interrupts
-  //set timer1 interrupt at 1kHz
+  cli();  // Stop interrupts
+  // Set timer1 interrupt at 1kHz
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
   // set timer count for 100Hz increments
-  OCR1A = 199;// = (16*10^5) / (1000*8) - 1
+  OCR1A = 2002;// = (16*10^5) / (100*8) - 1
   //had to use 16 bit timer1 for this bc 1999>255, but could switch to timers 0 or 2 with larger prescaler
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
@@ -42,7 +43,9 @@ void setup()
 
   pinMode(POTENTIOMETER, INPUT);
   pinMode(TILT_SENSOR, INPUT);
-  digitalWrite(TILT_SENSOR, HIGH); // turn on the pull-up resistor
+  pinMode(TILT_SENSOR, INPUT_PULLUP);
+  pinMode(HOLD_SWITCH, INPUT);
+  pinMode(HOLD_SWITCH, INPUT_PULLUP);
 
   pinMode(LED, OUTPUT);
 
@@ -57,48 +60,63 @@ void setup()
 
 void loop()
 {
-  display.setNumber(displayValue, 2);
+  display.setNumber(displayValue, 2); // Decimal point in the dead center, pretend it's "/"
 
-  int potentiometerValue = analogRead(POTENTIOMETER);
-  Serial.print(potentiometerValue);
-  Serial.print(": ");
-  
-  int mappedPotValue = map(potentiometerValue, MIN_POT, MAX_POT, MIN_RANGE, MAX_RANGE);
-  Serial.print(mappedPotValue);
-  Serial.print("; Tilt: ");
-
-  boolean tiltValue = !digitalRead(TILT_SENSOR);
-  Serial.print(tiltValue);
-
-  Serial.print("; Gen #: ");
-  Serial.println(displayValue / 100);
-
-  if(tiltValue)
+  boolean isHold = !digitalRead(HOLD_SWITCH);
+  Serial.print("Holding: ");
+  if(isHold)
   {
-    digitalWrite(LED, HIGH);
-    generateNumber(mappedPotValue);
+    Serial.println("YES; ");
   }
   else
   {
-    digitalWrite(LED, LOW);
+    Serial.print("NO; ");
+  }
+  
+  if(!isHold)
+  {
+    int potentiometerValue = analogRead(POTENTIOMETER);
+    Serial.print("Pot value: ");
+    Serial.print(potentiometerValue);
+    Serial.print(": ");
+    
+    int mappedPotValue = MAX_RANGE + MIN_RANGE - constrain(map(potentiometerValue, MIN_POT, MAX_POT, MIN_RANGE, MAX_RANGE), MIN_RANGE, MAX_RANGE); // Limit from 1 to 99; turn Right for higher numbers
+    Serial.print(mappedPotValue);
+    Serial.print("; Tilt: ");
+  
+    boolean tiltValue = !(digitalRead(TILT_SENSOR));
+    Serial.print(tiltValue);
+  
+    Serial.print("; Gen #: ");
+    Serial.println(displayValue / 100);
+  
+    if(tiltValue)
+    {
+      digitalWrite(LED, HIGH);
+      generateNumber(mappedPotValue);
+    }
+    else
+    {
+      digitalWrite(LED, LOW);
+    }
+  
+    displayValue -= (displayValue % 100);
+    displayValue += mappedPotValue;
   }
 
-  displayValue -= (displayValue % 100);
-  displayValue += mappedPotValue;
+  delay(100); // Run this loop at 10 Hz
 }
 
 void generateNumber(int range)
 { 
   int randomNumber = random(range + 1);
-  Serial.print("Generated Number: ");
-  Serial.println(randomNumber);
+//  Serial.print("Generated Number: ");
+//  Serial.println(randomNumber);
 
-  displayValue = randomNumber*100 + range;
+  displayValue = (randomNumber*100) + range;
 }
 
 ISR(TIMER1_COMPA_vect) // refresh display @ 100Hz
 {
   display.refreshDisplay();
 }
-
-
